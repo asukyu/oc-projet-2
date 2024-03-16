49bc7db6df9eeaf82abb7336a14417cf8aa2b3e3
Sarah BOYER <boyer.sarah@icloud.com>
2024-03-16 19:14:04 +0100

    Modifications page home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single } from './data';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +11,42 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+  id!: number;
+  country!: string;
+  single: any[] | undefined;
+  view: any[] = [700, 400];
 
-  constructor(private olympicService: OlympicService) {}
+ constructor(private olympicService: OlympicService) {
+  Object.assign(this, { single });
+ }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.country = "test";
   }
+ 
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: string = 'below';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  } 
 }
